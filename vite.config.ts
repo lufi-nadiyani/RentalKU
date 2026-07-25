0628e0b615f76dbf1b18d7362f040e9491986866
lufi-nadiyani <nadiyanilufi27@gmail.com>
2026-07-25 10:01:41 +0700

    Fix: Build ulang dengan gambar dan assets

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,13 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/RentalKu/', // Ganti dengan nama repo GitHub Anda
-
+  base: './', // Pakai relative path
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name].[hash].[ext]',
-        chunkFileNames: 'assets/[name].[hash].js',
-        entryFileNames: 'assets/[name].[hash].js',
-      },
-    },
+  },
+  server: {
+    port: 3000,
+    open: true,
   },
 });
