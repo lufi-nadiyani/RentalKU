ab37703e745b24d48549f9b2183419aaad8205bb
lufi-nadiyani <nadiyanilufi27@gmail.com>
2026-07-25 09:42:53 +0700

    Fix:RentalKu

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/RentalKU/', // Ganti dengan nama repo GitHub Anda
+  base: '/RentalKu/', // Ganti dengan nama repo GitHub Anda
 
   build: {
     outDir: 'dist',
